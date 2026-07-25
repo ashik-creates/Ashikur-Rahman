2c724f239332ef9985b75d3d47e856a862aa6569
ashikur rahman <ashikorrahman8900@gmail.com>
2026-07-25 12:54:11 +0600

    fixes some issues

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-screen flex flex-col overflow-x-hidden">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
